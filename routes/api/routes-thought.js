de95aae21504b20f4ea6179b9e8e83a3918127f4
Luca Urbanowicz <human.exp11@gmail.com>
2022-05-02 15:19:30 -0600

    pseudocode for routes

---
+++ routes/api/routes-thought.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+
+
+//GET all thoughts
+
+
+//GET single thought by _id
+
+
+//POST to create a new thought
+
+
+//PUT to update thought by _id
+
+
+//DELETE to remove thought by _id
+
+
+//POST to create reaction stored in single thought's reactions array
+
+
+//DELETE to remove reaction by reactionID
+
+
+
+module.exports = router;
---
+++ routes/api/routes-user.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+//GET all users
+
+
+//GET single user by _id and thought & friend data
+
+
+//POST a new user
+
+
+//PUT to update user by _id
+
+
+//DELETE to remove user by _id
+
+
+//POST to add new friend
+
+
+//DELETE to remove a friend
+
+
+module.exports = router;
