bbcd1a1c6f56ff13900f208d42e0ebb347b5107a
Luca Urbanowicz <human.exp11@gmail.com>
2022-05-02 15:09:24 -0600

    thought and user models

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const moment = require('moment');
+
+const User = model('User', UserSchema);
+
+//User 
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+},
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+//Create friendCount Virtual
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+module.exports = User;
